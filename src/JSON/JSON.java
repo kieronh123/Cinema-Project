57e1d9400daa9846f2b0b8b412fdded4697c505c
Callum Perks <ll15clp@leeds.ac.uk>
2018-03-14 11:18:00 +0000

    Added basic Seat, Ticket and user class. Added to and from json methods for all classes

--- src/JSON/JSON.java
+++ src/JSON/JSON.java
@@ -4,26 +4,55 @@ import com.google.gson.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
-import Tills.TillsSystem.sample.Movie;
+import Tills.TillsSystem.sample.*;
 
 public class JSON {
-    public static Movie[] getMovies(String json){
+    public static Movie[] moviesFromJson(String json){
         Gson gson = new Gson();
         Movie[] movies = gson.fromJson(json, Movie[].class);
         return movies;
     }
 
-    public static void addMovies(){
+    public static void moviesToJson(Movie[] movies){
         Gson gson = new Gson();
-        Movie[] movies = {new Movie(1, "movie 1", 5, 120, "director1", new ArrayList<String>(Arrays.asList("Actor1", "Actor 2")), "blurb1", "image1"), new Movie(2, "movie 2", 6, 110, "director2", new ArrayList<String>(Arrays.asList("Actor3", "Actor4")), "blurb2", "image2")};
         String json =  gson.toJson(movies);
+    }
+
+    public static Seat[] seatsFromJson(String json){
+        Gson gson = new Gson();
+        Seat[] seats = gson.fromJson(json, Seat[].class);
+        return seats;
+    }
+
+    public static void seatsToJson(Seat[] seats){
+        Gson gson = new Gson();
+        String json =  gson.toJson(seats);
         System.out.println(json);
     }
 
-    public static void main(String[] args) {
-        JSON.addMovies();
+    public static Ticket[] ticketsFromJson(String json){
+        Gson gson = new Gson();
+        Ticket[] tickets = gson.fromJson(json, Ticket[].class);
+        return tickets;
+    }
+
+    public static void ticketsToJson(Ticket[] tickets){
+        Gson gson = new Gson();
+        String json =  gson.toJson(tickets);
+        System.out.println(json);
+    }
+
+    public static User[] usersFromJson(String json){
+        Gson gson = new Gson();
+        User[] users = gson.fromJson(json, User[].class);
+        return users;
+    }
+
+    public static void usersToJson(User[] users){
+        Gson gson = new Gson();
+        String json =  gson.toJson(users);
+        System.out.println(json);
     }
 }
---
+++ src/Tills/TillsSystem/sample/User.java
@@ -0,0 +1,9 @@
+package Tills.TillsSystem.sample;
+
+public class User {
+    private int User_ID;
+    private String Username;
+    private String Password;
+
+    public User(){}
+}
