bd7378e9812b107d458f78d4552763e9c73914a3
Katie Mitchell <sc16km@leeds.ac.uk>
2018-03-16 18:39:37 +0000

    UI now runs with harness, need to add to build file

--- src/Tills/TillsSystem/sample/Main.java
+++ src/Tills/TillsSystem/sample/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Tills.TillsSystem.sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("selectedTicket.fxml"));
+    public void start(Stage primaryStage)throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        FileInputStream fileInputStream = new FileInputStream(new File("Tills/TillsSystem/sample/resources/selectedTicket.fxml"));
+        Parent root = loader.load(fileInputStream);
+
+        //Parent root = FXMLLoader.load(getClass().getResource("selectedTicket.fxml"));
         primaryStage.setTitle("Quail Cinema");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
