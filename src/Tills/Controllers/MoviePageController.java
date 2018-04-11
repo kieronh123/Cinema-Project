4aaa773af8d6e9b232e142414503fbf61067a386
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-11 17:09:00 +0100

    Movie names are now set from the database

--- src/Tills/Controllers/MoviePageController.java
+++ src/Tills/Controllers/MoviePageController.java
@@ -3,13 +3,40 @@ package Tills.Controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import JSON.JSON;
+import Tills.Harness;
+import Tills.Movie;
+
+
 public class MoviePageController {
 
     @FXML
-    Label label1;
+    private Label movie1;
+    @FXML
+    private Label movie2;
+    @FXML
+    private Label movie3;
+    @FXML
+    private Label movie4;
+    @FXML
+    private Label movie5;
+    @FXML
+    private Label movie6;
 
     @FXML
     private void initialize(){
-        label1.setText("Test text");
+        Harness harness = new Harness();
+        String moviesString = null;
+        Label[] labels = {movie1, movie2,movie3,movie4,movie5,movie6};
+        try {
+             moviesString = harness.sendGet("movies").toString();
+        }catch (Exception e){
+            System.err.println("Could not recieve data from database");
+        }
+        Movie[] movies = JSON.moviesFromJson(moviesString);
+
+        for (int i = 0; i < movies.length; i++) {
+            labels[i].setText(movies[i].getMovie_Name());
+        }
     }
 }
