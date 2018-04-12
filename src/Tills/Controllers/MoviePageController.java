ee7ef35dc0749edce3b241c851599568953e2180
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-12 15:18:02 +0100

    when ticket page is loaded, the time is now set according to the button that is pressed

--- src/Tills/Controllers/MoviePageController.java
+++ src/Tills/Controllers/MoviePageController.java
@@ -1,11 +1,20 @@
 package Tills.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import JSON.JSON;
 import Tills.Harness;
 import Tills.Movie;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class MoviePageController {
@@ -22,12 +31,13 @@ public class MoviePageController {
     private Label movie5;
     @FXML
     private Label movie6;
-    
+
     @FXML
     private void initialize(){
         Harness harness = new Harness();
         String moviesString = null;
         Label[] labels = {movie1, movie2,movie3,movie4,movie5,movie6};
+        //get movies list from the database
         try {
              moviesString = harness.sendGet("movies").toString();
         }catch (Exception e){
@@ -35,8 +45,47 @@ public class MoviePageController {
         }
         Movie[] movies = JSON.moviesFromJson(moviesString);
 
+        //set the text of each label to the title of the movie
         for (int i = 0; i < movies.length; i++) {
             labels[i].setText(movies[i].getMovie_Name());
         }
+    }
+
+    public void submitMovieChoice(ActionEvent event){
+        Button button = (Button)event.getSource();
+
+        switch (button.getParent().getParent().getId()){
+            case "movie1Grid":
+                System.out.println(movie1.getText());
+                break;
+            case "movie2Grid":
+                System.out.println(movie2.getText());
+                break;
+            case "movie3Grid":
+                System.out.println(movie3.getText());
+                break;
+            case "movie4Grid":
+                System.out.println(movie4.getText());
+                break;
+            case "movie5Grid":
+                System.out.println(movie5.getText());
+                break;
+            case "movie6Grid":
+                System.out.println(movie6.getText());
+                break;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../ticketType.fxml"));
+            TicketPageController controller = new TicketPageController(button.getText());
+            loader.setController(controller);
+            Parent parent = (Parent)loader.load();
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(new Scene(parent));
+            window.show();
+        }catch(IOException e){
+            System.err.println("Could not load page");
+        }
+
     }
 }
