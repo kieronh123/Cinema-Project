e9c05e07f0a08ff1b85ed3e0bd8a2b24d956eee3
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-18 17:57:40 +0100

    Movies page now works with database restructure

--- src/Tills/Controllers/MoviePageController.java
+++ src/Tills/Controllers/MoviePageController.java
@@ -15,9 +15,12 @@ import Tills.Harness;
 import Tills.Movie;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
 
 
 public class MoviePageController {
@@ -35,29 +38,55 @@ public class MoviePageController {
     @FXML
     private Label movie6;
 
+    Label[] movieLabels;
+
     @FXML
     private void initialize() {
+        Map<Integer, ArrayList<WhatsOn>> movieMap = new HashMap<>();
+
         String whatsOnString = null;
-        Label[] labels = {movie1, movie2, movie3, movie4, movie5, movie6};
+        movieLabels = new Label[]{movie1, movie2, movie3, movie4, movie5, movie6};
         try {
             //get the list of screenings from the database
             whatsOnString = Harness.sendGet("whatson").toString();
             WhatsOn[] whatsOn = JSON.whatsOnFromJson(whatsOnString);
 
-            //set the text of each label to the title of the movie
-            for (int i = 0; i < 6; i++) {
-                String movieString = Harness.sendGet("movies/" + whatsOn[i].getScreening_ID()).toString();
-                Movie movie = JSON.movieFromJson(movieString);
-                labels[i].setText(movie.getMovie_Name());
+            for (int i = 0; i < whatsOn.length; i++) {
+                if(!movieMap.containsKey(whatsOn[i].getMovie_ID())){
+                    movieMap.put(whatsOn[i].getMovie_ID(),new ArrayList<>(Arrays.asList(whatsOn[i])));
+                } else {
+                    ArrayList<WhatsOn> list = movieMap.get(whatsOn[i].getMovie_ID());
+                    list.add(whatsOn[i]);
+                    System.out.println(movieMap.get(whatsOn[i].getMovie_ID()));
+                }
+            }
+
+            int movieCount = 0;
+            for(ArrayList<WhatsOn> list : movieMap.values()){
+                int screeningCount = 0;
+                setMovieLabel(movieCount, list.get(0));
+                movieCount++;
+                for (WhatsOn whatson : list){
+                    Label label = movieLabels[screeningCount/3];
+                    GridPane gridPane = (GridPane)label.getParent().getParent();
+                    HBox hbox = (HBox)gridPane.getChildren().get(1);
+                    VBox vBox = (VBox)hbox.getChildren().get(screeningCount);
+                    Label screenLabel = (Label)vBox.getChildren().get(1);
+                    Button button = (Button)vBox.getChildren().get(0);
+                    screenLabel.setText("Screen " + whatson.getScreen_ID());
+                    button.setText(whatson.getStart_Time().substring(11,16));
+                    screeningCount++;
+                }
             }
         } catch (Exception e) {
+            e.printStackTrace();
             System.err.println("Could not recieve data from database");
         }
     }
 
     public void submitMovieChoice(ActionEvent event) {
         Button button = (Button) event.getSource();
-        GridPane movie = (GridPane) button.getParent().getParent();
+        GridPane movie = (GridPane) button.getParent().getParent().getParent();
         String name = null;
 
         //retrieve the name of the selected movie
@@ -77,6 +106,17 @@ public class MoviePageController {
             window.show();
         } catch (IOException e) {
             System.err.println("Could not load page");
+        }
+    }
+
+
+    public void setMovieLabel(int index, WhatsOn whatsOn){
+        try {
+            String movieString = Harness.sendGet("movies/" + whatsOn.getMovie_ID()).toString();
+            Movie movie = JSON.movieFromJson(movieString);
+            movieLabels[index].setText(movie.getMovie_Name());
+        }catch (Exception e){
+            System.err.println("Failed to retrieve movie");
         }
     }
 }
