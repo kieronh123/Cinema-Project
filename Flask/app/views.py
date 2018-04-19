98d71f8101c48c4e9a55bb5bf141cac58c2712d8
Katie Mitchell <sc16km@leeds.ac.uk>
2018-04-19 12:39:42 +0100

    Changes to views.py

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -6,10 +6,19 @@ from flask import render_template
 def index():
 
     number = []
+    #input from screening table
+    screenings = []
+
+    #screenings.append();
+    #for i in range(0,len(screenings)):
+        #Call movie information
+        #movie = []
+        #movie.append()
+        #screenings.append(movie)
     for i in range(0,10):
         number.append(i)
-    return render_template('index.html');
-    #return render_template('index.html',msg="Hello Katie",number=number, smell=False);
+    #return render_template('index.html');
+    return render_template('index_old.html',msg="Hello Katie",number=number, smell=False);
 
 
 @app.route('/tickets')
