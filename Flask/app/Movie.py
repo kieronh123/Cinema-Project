88e951d994650a5ada516aa971737e08d05b3f26
Alex <sc16ajdh@leeds.ac.uk>
2018-04-29 20:41:24 +0100

    Purged obselete files and folders

--- Flask/app/Movie.py
+++
@@ -1,9 +0,0 @@
-class Movie:
-    def __init__(self, Movie_ID, Movie_Name, Movie_Rating, Movie_Runtime, Movie_Info, Movie_image):
-        self.Movie_ID = Movie_ID
-        self.Movie_Name = Movie_Name
-        self.Movie_Rating = Movie_Rating
-        self.Movie_Runtime = Movie_Runtime
-        self.Movie_Info = Movie_Info
-        self.Movie_image = Movie_image
-
--- Flask/app/User.py
+++
@@ -1,5 +0,0 @@
-class User:
-    def __init__(self, User_ID, Username, Password):
-        self.User_ID = User_ID
-        self.Username = Username
-        self.Password = Password
