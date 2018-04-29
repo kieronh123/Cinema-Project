4d8127633d6b4e1164a61427461da588caeb32f8
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-29 19:02:17 +0100

    modified runTests script

--- Flask/app/tests/runTests.sh
+++ Flask/app/tests/runTests.sh
@@ -2,7 +2,6 @@
 python3 registration_test.py
 python3 login_test.py
 python3 navigation_bar_test.py
-python3 payment_test.py
 python3 ticket_type_test.py
 python3 time_select_test.py
 source Rest_Api_Test.sh
