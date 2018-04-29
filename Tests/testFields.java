32e516792184e45497492d31be75047466172cdd
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-29 21:18:44 +0100

    Robot tests now run through ant

--- Tests/testFields.java
+++ Tests/testFields.java
@@ -1,10 +1,10 @@
-package Tills.Controllers;
 import org.junit.Test;
 import Tills.*;
 import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import Tills.Controllers.*;
 
 
 /**
@@ -25,34 +25,26 @@ public class testFields {
 //  //Cannot have negative or over bounded fields
   @Test
   public void testFields() {
-    PaymentPageController fields = new PaymentPageController("13:00", "Batman", "25", 10.00);
+    PaymentPageController fields = new PaymentPageController("13:00", "Batman", "25", 10.00, "5", "7", 6);
     assertThat(fields.getTime(), is("13:00"));
     assertThat(fields.getName(), is("Batman"));
     assertThat(fields.getSeat(), is("25"));
     assertThat(fields.getTotal(), is(10.00));
 
   }
-  @Test
-  public void testFields() {
-    TicketPageController pages = new TicketPageController("12:00", "Superman", 14, "18");
-    assertThat(pages.getTime(), is("12:00"));
-    assertThat(pages.getFilmName(), is("Superman"));
-    assertThat(pages.getScreeningID(), is(14));
-    assertThat(pages.getAge(), is("18"));
-  }
 
   @Test(expected = IllegalArgumentException.class)
   public void invalidPayment() {
-    new PaymentPageController("13:00", "Batman", "25", -5.00);
+    new PaymentPageController("13:00", "Batman", "25", -5.00, "5", "7", 6);
   }
 
   // Testing that fields are what it is meant to be
   @Test
   public void equality() {
-    PaymentPageController fields = new PaymentPageController("13:00", "Batman", "25", 10.00);
+    PaymentPageController fields = new PaymentPageController("13:00", "Batman", "25", 10.00, "2", "3", 7);
     assertTrue(fields.equals(fields));
-    assertFalse(fields.equals(new PaymentPageController("13:43", "SuperMan", "32", 1.00)));
-    assertTrue(fields.equals(new PaymentPageController("13:00", "Batman", "25", 10.00)));
+    assertFalse(fields.equals(new PaymentPageController("13:43", "SuperMan", "32", 10.00, "2", "3", 7)));
+    assertTrue(fields.equals(new PaymentPageController("13:00", "Batman", "25", 10.00, "2", "3", 7)));
   }
 
   private ArrayList testList;
@@ -107,6 +99,6 @@ public class testFields {
       }
     }).start();
 
-    Main.main();
+//    Main.main();
   }
-}
+  }
