310d3d282c046d359978861f21a7643b948ad7f3
Callum Perks <ll15clp@leeds.ac.uk>
2018-04-29 23:54:05 +0100

    Fixed vip tests for website

--- Flask/app/tests/runTests.sh
+++ Flask/app/tests/runTests.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-source Setup_Database_Test.sh
-cd ../../..
-source Run_Flask.sh
 python3 registration_test.py
 python3 login_test.py
 python3 navigation_bar_test.py
 python3 ticket_type_test.py
-python3 time_select_test.py
+python3 vip_ticket_type_test.py
 source Rest_Api_Test.sh
