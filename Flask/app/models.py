6b3ca880a6affa607e65ca833b693ea508ee7903
Alex <sc16ajdh@leeds.ac.uk>
2018-04-29 21:27:09 +0100

    Added test database

--- Flask/app/models.py
+++ Flask/app/models.py
@@ -1,3 +1,5 @@
+##Classes that provide objects for the SQL tables to access
+#WhatsOn table
 class WhatsOn ():
     def __init__(self, Screening_ID, Movie_ID, Screen_ID, Start_Time,):
         self.Screening_ID = Screening_ID
@@ -9,6 +11,7 @@ class WhatsOn ():
     def __str__(self):
         return "%s %s %s %s" % (self.Screening_ID,self.Movie_ID,self.Screen_ID,self.Start_Time)
 
+#Movie table
 class Movie ():
     def __init__(self, Movie_ID, Movie_Name, Movie_Rating, Movie_Runtime, Movie_Info, Movie_image, Movie_Director, Movie_Actors):
         self.Movie_ID = Movie_ID
@@ -20,19 +23,21 @@ class Movie ():
         self.Movie_Director = Movie_Director
         self.Movie_Actors = Movie_Actors
 
-
+#Users table
 class User ():
     def __init__(self, User_ID, Username, Password):
         self.User_ID = User_ID
         self.Username = Username
         self.Password = Password
 
+#Bookings table
 class Booking ():
     def __init__(self, Screening_ID,Row_Num,Column_Num):
         self.Screening_ID = Screening_ID
         self.Row_Num = Row_Num
         self.Column_Num = Column_Num
 
+#Card details table
 class CardDetails ():
     def __init__(self, User_ID,Card_Name,Card_Number,Card_SortCode,Card_SecurityCode):
         self.User_ID = User_ID
